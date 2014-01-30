17fc0dafe617d5aae652f5869eaab6324e515540
mhoonkang <mhoonkang@wustl.edu>
2014-01-30 14:24:16 -0600

    Revert "Revert 6223ed6..6ec10c3"
    This reverts commit b67a213ac47350a792585edd3c3cbcee9160efc6.

--- ClassExample.R
+++ ClassExample.R
@@ -1,9 +1,14 @@
-
-
 print("Hello World")
 
 
 print("A new line")
 
 
-print("Change")
+print("A thir line")
+
+print("More lines, more problems")
+
+
+print("More problems, More lines")
+
+apply(git,2,EVERYTHING)
