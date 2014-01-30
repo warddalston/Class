b67a213ac47350a792585edd3c3cbcee9160efc6
warddalston <macuser@seiglel016imac-11.wustl.edu>
2014-01-30 14:22:55 -0600

    Revert 6223ed6..6ec10c3
    This rolls back to commit 6223ed68578e29466db465f423b9f20210b29145.

--- ClassExample.R
+++ ClassExample.R
@@ -1,14 +1,9 @@
+
+
 print("Hello World")
 
 
 print("A new line")
 
 
-print("A thir line")
-
-print("More lines, more problems")
-
-
-print("More problems, More lines")
-
-apply(git,2,EVERYTHING)
+print("Change")
